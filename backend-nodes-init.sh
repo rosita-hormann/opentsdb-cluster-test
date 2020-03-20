7ee3824abd29cda4d86152df97db44dd898f2ad2
lil-rose <rosita.hormannlobos@gmail.com>
2020-03-20 16:25:26 -0300

    Fixed Hadoop cluster

--- backend-nodes-init.sh
+++ backend-nodes-init.sh
@@ -36,60 +36,52 @@ docker run -d \
 	--env ZOO_SERVERS="server.1=zookeeper-node1:2888:3888 server.2=zookeeper-node2:2888:3888 server.3=zookeeper-node3:2888:3888" \
 	zookeeper:3.4
 
+	# --replicas 1 \
+	# --detach=true \
+	# --endpoint-mode dnsrr \
+	# --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+	# --mount type=bind,source=/data/hadoop/config,target=/config/hadoop \
+	# --mount type=bind,source=/data/hadoop/hdfs/master,target=/tmp/hadoop-root \
+	# --mount type=bind,source=/data/hadoop/logs/master,target=/usr/local/hadoop/logs \
 # HDFS (Hadoop) servers:
 docker run -d \
 	--name hadoop-master \
 	--hostname hadoop-master \
 	--network hdfs-net \
-	--replicas 1 \
-	--detach=true \
-	--endpoint-mode dnsrr \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--mount type=bind,source=/data/hadoop/config,target=/config/hadoop \
-	--mount type=bind,source=/data/hadoop/hdfs/master,target=/tmp/hadoop-root \
-	--mount type=bind,source=/data/hadoop/logs/master,target=/usr/local/hadoop/logs \
+	-p 16010:16010 \
 	newnius/hadoop:2.7.4
-
-# Notice: --endpoint-mode dnsrr is required
 
 docker run -d \
 	--name hadoop-slave1 \
 	--hostname hadoop-slave1 \
 	--network hdfs-net \
-	--replicas 1 \
-	--detach=true \
-	--endpoint-mode dnsrr \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--mount type=bind,source=/data/hadoop/config,target=/config/hadoop \
-	--mount type=bind,source=/data/hadoop/hdfs/slave1,target=/tmp/hadoop-root \
-	--mount type=bind,source=/data/hadoop/logs/slave1,target=/usr/local/hadoop/logs \
 	newnius/hadoop:2.7.4
 
 docker run -d \
 	--name hadoop-slave2 \
 	--hostname hadoop-slave2 \
 	--network hdfs-net \
-	--replicas 1 \
-	--detach=true \
-	--endpoint-mode dnsrr \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--mount type=bind,source=/data/hadoop/config,target=/config/hadoop \
-	--mount type=bind,source=/data/hadoop/hdfs/slave2,target=/tmp/hadoop-root \
-	--mount type=bind,source=/data/hadoop/logs/slave2,target=/usr/local/hadoop/logs \
 	newnius/hadoop:2.7.4
 
 docker run -d \
 	--name hadoop-slave3 \
 	--hostname hadoop-slave3 \
 	--network hdfs-net \
-	--replicas 1 \
-	--detach=true \
-	--endpoint-mode dnsrr \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--mount type=bind,source=/data/hadoop/config,target=/config/hadoop \
-	--mount type=bind,source=/data/hadoop/hdfs/slave3,target=/tmp/hadoop-root \
-	--mount type=bind,source=/data/hadoop/logs/slave3,target=/usr/local/hadoop/logs \
 	newnius/hadoop:2.7.4
+
+# Now we restart Hadoop master node
+docker exec -it hadoop-master /bin/bash
+
+# stop HDFS services
+sbin/stop-dfs.sh
+
+# format HDFS meta data
+bin/hadoop namenode -format
+
+# restart HDFS services
+sbin/start-dfs.sh
+
+# Then you must logout of hadoop-master container
 
 # HBase nodes:
 docker run -d \
