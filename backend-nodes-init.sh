7620ff9864672ddb6ed37f8449bce2ea21e2b758
lil-rose <rosita.hormannlobos@gmail.com>
2020-03-25 16:24:14 -0300

    Definitive backend nodes script

--- backend-nodes-init.sh
+++ backend-nodes-init.sh
@@ -6,14 +6,11 @@ docker network create hdfs-net
 
 # ZooKeeper nodes:
 
-	# --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	# --mount type=bind,source=/data/zookeeper/data/node1,target=/data \
-	# --mount type=bind,source=/data/zookeeper/logs/node1,target=/datalog \
 docker run -d \
 	--name zookeeper-node1 \
 	--restart always \
-	--hostname zookeeper-node1 \
 	--network hdfs-net \
+	--hostname zookeeper-node1 \
 	--env ZOO_MY_ID=1 \
 	--env ZOO_SERVERS="server.1=zookeeper-node1:2888:3888 server.2=zookeeper-node2:2888:3888 server.3=zookeeper-node3:2888:3888" \
 	zookeeper:3.4
@@ -21,8 +18,8 @@ docker run -d \
 docker run -d \
 	--name zookeeper-node2 \
 	--restart always \
-	--hostname zookeeper-node2 \
 	--network hdfs-net \
+	--hostname zookeeper-node2 \
 	--env ZOO_MY_ID=2 \
 	--env ZOO_SERVERS="server.1=zookeeper-node1:2888:3888 server.2=zookeeper-node2:2888:3888 server.3=zookeeper-node3:2888:3888" \
 	zookeeper:3.4
@@ -30,43 +27,40 @@ docker run -d \
 docker run -d \
 	--name zookeeper-node3 \
 	--restart always \
-	--hostname zookeeper-node3 \
 	--network hdfs-net \
+	--hostname zookeeper-node3 \
 	--env ZOO_MY_ID=3 \
 	--env ZOO_SERVERS="server.1=zookeeper-node1:2888:3888 server.2=zookeeper-node2:2888:3888 server.3=zookeeper-node3:2888:3888" \
 	zookeeper:3.4
 
-	# --replicas 1 \
-	# --detach=true \
-	# --endpoint-mode dnsrr \
-	# --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	# --mount type=bind,source=/data/hadoop/config,target=/config/hadoop \
-	# --mount type=bind,source=/data/hadoop/hdfs/master,target=/tmp/hadoop-root \
-	# --mount type=bind,source=/data/hadoop/logs/master,target=/usr/local/hadoop/logs \
 # HDFS (Hadoop) servers:
 docker run -d \
 	--name hadoop-master \
-	--hostname hadoop-master \
+	--restart always \
 	--network hdfs-net \
+	--hostname hadoop-master \
 	-p 16010:16010 \
 	newnius/hadoop:2.7.4
 
 docker run -d \
 	--name hadoop-slave1 \
-	--hostname hadoop-slave1 \
+	--restart always \
 	--network hdfs-net \
+	--hostname hadoop-slave1 \
 	newnius/hadoop:2.7.4
 
 docker run -d \
 	--name hadoop-slave2 \
-	--hostname hadoop-slave2 \
+	--restart always \
 	--network hdfs-net \
+	--hostname hadoop-slave2 \
 	newnius/hadoop:2.7.4
 
 docker run -d \
 	--name hadoop-slave3 \
-	--hostname hadoop-slave3 \
+	--restart always \
 	--network hdfs-net \
+	--hostname hadoop-slave3 \
 	newnius/hadoop:2.7.4
 
 # Now we restart Hadoop master node
@@ -93,26 +87,30 @@ sbin/start-dfs.sh
 	# --mount type=bind,source=/data/hbase/logs/master,target=/usr/local/hbase/logs \
 docker run -d \
 	--name hbase-master \
-	--hostname hbase-master \
+	--restart always \
 	--network hdfs-net \
+	--hostname hbase-master \
 	newnius/hbase:1.2.6
 
 docker run -d \
 	--name hbase-slave1 \
-	--hostname hbase-slave1 \
+	--restart always \
 	--network hdfs-net \
+	--hostname hbase-slave1 \
 	newnius/hbase:1.2.6
 
 docker run -d \
 	--name hbase-slave2 \
-	--hostname hbase-slave2 \
+	--restart always \
 	--network hdfs-net \
+	--hostname hbase-slave2 \
 	newnius/hbase:1.2.6
 
 docker run -d \
 	--name hbase-slave3 \
-	--hostname hbase-slave3 \
+	--restart always \
 	--network hdfs-net \
+	--hostname hbase-slave3 \
 	newnius/hbase:1.2.6
 # First we copy create_table.sh (from OpenTSDB build) on the hbase-master container
 docker cp create_table.sh hbase-master:create_table.sh
@@ -122,5 +120,4 @@ docker exec -it hbase-master /bin/bash
 
 # inside hbase-master container
 bin/start-hbase.sh
-cd /
 env COMPRESSION=NONE HBASE_HOME=/usr/local/hbase /create_table.sh
