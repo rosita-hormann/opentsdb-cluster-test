a656c21b2eaab44d7167f32053d6dffa63c5791b
lil-rose <rosita.hormannlobos@gmail.com>
2020-03-25 16:23:51 -0300

    delete-cluster.sh ordered in a better way

--- delete-cluster.sh
+++ delete-cluster.sh
@@ -2,40 +2,26 @@
 
 #Stopping containers
 
-docker stop opentsdb-1
-docker stop opentsdb-2
+docker stop opentsdb-1 opentsdb-2
 
-docker stop hbase-master
-docker stop hbase-slave1
-docker stop hbase-slave2
-docker stop hbase-slave3
+docker stop hbase-master hbase-slave1 hbase-slave2 hbase-slave3
 
-docker stop hadoop-master
-docker stop hadoop-slave1
-docker stop hadoop-slave2
-docker stop hadoop-slave3
+docker stop hadoop-master hadoop-slave1 hadoop-slave2 hadoop-slave3
 
-docker stop zookeeper-node1
-docker stop zookeeper-node2
-docker stop zookeeper-node3
+docker stop zookeeper-node1 zookeeper-node2 zookeeper-node3
+
+
+# Remove all docker containers that are stopped
+docker container rm $(docker container ls -aq)
 
 #Removing containers
-docker rm opentsdb-1
-docker rm opentsdb-2
+docker rm opentsdb-1 opentsdb-2
 
-docker rm hbase-master
-docker rm hbase-slave1
-docker rm hbase-slave2
-docker rm hbase-slave3
+docker rm hbase-master hbase-slave1 hbase-slave2 hbase-slave3
 
-docker rm hadoop-master
-docker rm hadoop-slave1
-docker rm hadoop-slave2
-docker rm hadoop-slave3
+docker rm hadoop-master hadoop-slave1 hadoop-slave2 hadoop-slave3
 
-docker rm zookeeper-node1
-docker rm zookeeper-node2
-docker rm zookeeper-node3
+docker rm zookeeper-node1 zookeeper-node2 zookeeper-node3
 
 #Removing network
 docker network rm hdfs-net
