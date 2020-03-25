61a4cdf641bdfe07fa5e0e3658fe83ccad966aea
lil-rose <rosita.hormannlobos@gmail.com>
2020-03-25 16:32:11 -0300

    frontend nodes script updated to use my own docker image

--- frontend-nodes-init.sh
+++ frontend-nodes-init.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-
 docker run -d -p 4242:4242 \
-	-e TIMEZONE=America/Santiago \
-	-e ZK_QUORUM=hbase-master,hbase-slave1,hbase-slave2,hbase-slave3 \
-	-e COMPACTION=False \
 	--name opentsdb-1 \
 	--hostname opentsdb-1 \
 	--network hdfs-net \
-	olegfedoseev/opentsdb
+	lilrose/opentsdb:0.1
 
 docker run -d -p 4243:4242 \
-	-e TIMEZONE=America/Santiago \
-	-e ZK_QUORUM=hbase-master,hbase-slave1,hbase-slave2,hbase-slave3 \
-	-e COMPACTION=False \
 	--name opentsdb-2 \
 	--hostname opentsdb-2 \
 	--network hdfs-net \
-	olegfedoseev/opentsdb
+	lilrose/opentsdb:0.1
