bac660a3e73dfb1dace0342c56b84d92bd983d0e
lil-rose <rosita.hormannlobos@gmail.com>
2020-04-07 17:47:46 -0400

    tests with javascript library updated

--- insert_data_example.js
+++ insert_data_example.js
@@ -1,19 +1,22 @@
 var TsdbClient = require('opentsdb-node-client');
 var myTsdbClient = new TsdbClient({
     host: 'http://localhost',
-    port: '4242'
+    port: '8080'
 });
 currentDate = new Date().getTime()
-for(var i = 0; i<1000; i++){
+
+for(var i = 0; i<3600; i++){
     myTsdbClient.putMetric(
-        'example', // metric name
+        'other1', // metric name
         currentDate, // timestamp
         Math.floor(Math.random() * 1000), { // measured value and its tags
             host: "host_example",
             bit: Math.floor(Math.random() * 1000) % 2
         }
-    ).then(function() {
-        console.log('putMetric', r);
-    });
-    currentDate = currentDate  + 500
+    )
+    .then(r => console.log('putMetric', r)
+    .then(e => console.error(e))
+    );
+    currentDate = currentDate  + 1000
+
 }
